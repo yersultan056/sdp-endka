ec06b6803be76f9bd38aae664c3b790728046286
yersultan056 <230129@astanait.edu.kz>
2024-11-05 23:23:30 +0500

    create project

---
+++ src/main/java/kz/sdppj/sdpendka/SdpendkaApplication.java
@@ -0,0 +1,13 @@
+package kz.sdppj.sdpendka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdpendkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdpendkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/sdppj/sdpendka/SdpendkaApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sdppj.sdpendka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdpendkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
